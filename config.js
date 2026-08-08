c2aa1a41996211955b926df689296ca204c7ed58
GEM_VPNBOT <166546200+FDRIVPN@users.noreply.github.com>
2026-08-08 19:42:06 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export default {
   port: process.env.PORT || 8080,
   maxPlayers: 50,
-  world: { width: 1000, height: 1000 },
+  world: { width: 10000, height: 10000 },
   pingInterval: 5000,
   vehicle: { maxSeats: 4 },
   banFile: './data/bans.json'
