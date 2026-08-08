5c7027688d6db1350aa2ab705134b95104aebd8f
GEM_VPNBOT <166546200+FDRIVPN@users.noreply.github.com>
2026-08-08 19:42:40 +0330

    Update Player.js

--- Player.js
+++ Player.js
@@ -1,65 +1,27 @@
-import { randomUUID } from "crypto";
+import { randomUUID } from 'crypto';
 
-class Player {
-    constructor(id = null) {
-        this.id = id || randomUUID();
-        this.name = "Player";
-        this.job = "None";
-        this.x = 0;
-        this.y = 0;
-        this.z = 0;
-        this.rot = 0;
-        this.animation = "idle";
-        this.connected = true;
-        this.joinTime = Date.now();
-    }
+export default class Player {
+  constructor(ws) {
+    this.id = randomUUID();
+    this.ws = ws;
+    this.name = '';
+    this.job = '';
+    this.position = { x: 0, y: 0 };
+    this.rotation = 0;
+    this.vehicleId = null;
+    this.seatIndex = null;
+    this.isBanned = false;
+  }
 
-    // به‌روزرسانی موقعیت
-    updatePosition(x, y, z, rot, animation) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.rot = rot;
-        this.animation = animation || this.animation;
+  send(data) {
+    if (this.ws.readyState === 1) {
+      this.ws.send(JSON.stringify(data));
     }
+  }
 
-    // تنظیم نام
-    setName(name) {
-        if (name && name.length > 0 && name.length <= 20) {
-            this.name = name;
-            return true;
-        }
-        return false;
+  close(reason) {
+    if (this.ws.readyState === 1) {
+      this.ws.close(1000, reason);
     }
-
-    // تنظیم شغل
-    setJob(job) {
-        const validJobs = ["None", "Warrior", "Mage", "Archer", "Healer", "Assassin"];
-        if (validJobs.includes(job)) {
-            this.job = job;
-            return true;
-        }
-        return false;
-    }
-
-    // گرفتن اطلاعات بازیکن برای ارسال به کلاینت
-    toJSON() {
-        return {
-            id: this.id,
-            name: this.name,
-            job: this.job,
-            x: this.x,
-            y: this.y,
-            z: this.z,
-            rot: this.rot,
-            animation: this.animation
-        };
-    }
-
-    // بررسی اینکه بازیکن فعال است
-    isActive() {
-        return this.connected && (Date.now() - this.joinTime < 3600000); // 1 ساعت
-    }
+  }
 }
-
-export default Player;
