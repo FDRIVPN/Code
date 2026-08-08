05c279746214038f037a59e823d0d26eb0abf76e
GEM_VPNBOT <166546200+FDRIVPN@users.noreply.github.com>
2026-08-08 19:42:59 +0330

    Create Vehicle.js

---
+++ Vehicle.js
@@ -0,0 +1,52 @@
+export default class Vehicle {
+  constructor(ownerId, name, job, position, rotation, steering) {
+    this.id = ownerId;
+    this.ownerId = ownerId;
+    this.name = name;
+    this.job = job;
+    this.position = { ...position };
+    this.rotation = rotation;
+    this.steering = steering || 0;
+    this.seats = [ownerId, null, null, null];
+  }
+
+  isSeatAvailable(seatIndex) {
+    return seatIndex >= 0 && seatIndex < 4 && this.seats[seatIndex] === null;
+  }
+
+  addPassenger(playerId, seatIndex) {
+    if (seatIndex === 0) return false;
+    if (!this.isSeatAvailable(seatIndex)) return false;
+    this.seats[seatIndex] = playerId;
+    return true;
+  }
+
+  removePassenger(playerId) {
+    for (let i = 0; i < this.seats.length; i++) {
+      if (this.seats[i] === playerId) {
+        this.seats[i] = null;
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  updateState(position, rotation, steering) {
+    if (position) this.position = { ...position };
+    if (rotation !== undefined) this.rotation = rotation;
+    if (steering !== undefined) this.steering = steering;
+  }
+
+  getState() {
+    return {
+      id: this.id,
+      ownerId: this.ownerId,
+      name: this.name,
+      job: this.job,
+      position: this.position,
+      rotation: this.rotation,
+      steering: this.steering,
+      seats: this.seats
+    };
+  }
+}
